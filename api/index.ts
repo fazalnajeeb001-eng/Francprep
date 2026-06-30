6fef0780162a4d0936f5cec265739580b158f97d
x <x@x.com>
2026-06-30 07:43:29 +0000

    switch frontend to Railway with Node.js SSR server

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-// Vercel serverless function adapter for TanStack Start SSR
-import server from '../dist/server/server.js'
-
-export default async function handler(req: Request): Promise<Response> {
-  return server.fetch(req)
-}
