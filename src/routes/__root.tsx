60862860f22db589dfd0f6e8ceebd2134afe878c
x <x@x.com>
2026-07-03 06:37:16 +0000

    fix: update browser tab title to FrancPrep

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,6 +15,7 @@ export const Route = createRootRoute({
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { title: "FrancPrep - TCF/TEF Exam Prep & French Coaching" },
       { title: "FrancPrep" },
     ],
     links: [{ rel: "stylesheet", href: appCss }],
